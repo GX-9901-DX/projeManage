4f116ae421303f8681d176c65dab4127d3aeb516
GX-9901-DX <msahiro.yoshino@gmail.com>
2023-09-13 13:11:05 +0900

    スプラッシュ画面追加

--- app/src/main/java/com/example/projemanage/SplashActivity.kt
+++ app/src/main/java/com/example/projemanage/SplashActivity.kt
@@ -1,11 +1,31 @@
 package com.example.projemanage
 
+import android.graphics.Typeface
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        @Suppress("deprecation")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        // TODO フリーのフォントを使ってみよう
+//        val typeface:Typeface = Typeface.createFromAsset(assets, "")
+//        val tv_app_name: TextView = findViewById(R.id.tv_app_name)
+//        tv_app_name.typeface =typeface
     }
 }
